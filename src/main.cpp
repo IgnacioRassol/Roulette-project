9db2022e7f91bc54c2dd02d91d69636808120bea
Ignacio Rassol <ignaciorassol@gmail.com>
2024-08-28 01:59:47 -0300

    Add comment to main.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include "simulation.h"
 
 int main() {
-    
+    // We wrap this in a try-catch block in case we fail to write to a file when
+    // logging player activity.
     try {
         default_simulation();
     }
